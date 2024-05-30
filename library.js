40dddc2067b5fac6813d760e01cbf6fd0b1f0934
Victor Quintero <131626735+VictorDQuintero@users.noreply.github.com>
2024-05-29 21:56:12 -0600

    Add code for stretch function

--- library.js
+++ library.js
@@ -23,7 +23,7 @@ const library = {
     p01: { id: "p01", name: "Coding Music", tracks: ["t01", "t02"] },
     p02: { id: "p02", name: "Other Playlist", tracks: ["t03"] },
   },
-  printPlaylists: function () {
+  printPlaylists: function() {
     // prints a list of all playlists, in the form:
     // p01: Coding Music - 2 tracks
     // p02: Other Playlist - 1 tracks
@@ -35,7 +35,7 @@ const library = {
     }
   },
 
-  printTracks: function () {
+  printTracks: function() {
     for (const tracksKey in this.tracks) {
       console.log(
         `${this.tracks[tracksKey].id}: ${this.tracks[tracksKey].name} by ${this.tracks[tracksKey].artist} (${this.tracks[tracksKey].album}) `
@@ -43,7 +43,7 @@ const library = {
     }
   },
 
-  printPlaylist: function (playlistId) {
+  printPlaylist: function(playlistId) {
     // let playlistsKeyArr = Object.keys(this.playlists); //creates an array of the playlists inside of the object
     // let counter = 1; // initializes a counter at 1
 
@@ -88,7 +88,7 @@ const library = {
     }
   },
 
-  addTrackToPlaylist: function (trackId, playlistId) {
+  addTrackToPlaylist: function(trackId, playlistId) {
     // const tracksKeysArr = Object.keys(this.tracks);
     // let counter = 1;
     // let playlistsKeyArr = Object.keys(this.playlists);
@@ -136,8 +136,8 @@ const library = {
 
     //TO DO: rearreange the array accordingly
 
-    if (this.playlists[playlistId]) {
-      if (this.tracks[trackId]) {
+    if (this.playlists[playlistId].id) {
+      if (this.tracks[trackId].id) {
         for (const track of Object.values(this.playlists[playlistId].tracks)) {
           if (track === trackId) {
             console.log(`${trackId} is already in ${playlistId}`);
@@ -154,13 +154,13 @@ const library = {
     this.playlists[playlistId].tracks.push(trackId);
   },
 
-  generateUid: function () {
+  generateUid: function() {
     return Math.floor((1 + Math.random()) * 0x10000)
       .toString(16)
       .substring(1);
   },
 
-  addTrack: function (name, artist, album) {
+  addTrack: function(name, artist, album) {
     const trackId = this.generateUid();
 
     this.tracks[trackId] = {
@@ -170,7 +170,7 @@ const library = {
       album: album,
     };
   },
-  addPlaylist: function (name) {
+  addPlaylist: function(name) {
     const playlistId = this.generateUid();
 
     this.playlists[playlistId] = {
@@ -178,6 +178,17 @@ const library = {
       name: name,
       tracks: [],
     };
+  },
+  printSearchResults: function(query) {
+    for (const tracksKey in this.tracks) {
+      for (const track of Object.keys(this.tracks[tracksKey])) {
+        if (this.tracks[tracksKey][track].search(query) !== -1) {
+          console.log(
+            `${this.tracks[tracksKey].id}: ${this.tracks[tracksKey].name} by ${this.tracks[tracksKey].artist} (${this.tracks[tracksKey].album}) `
+          );
+        }
+      }
+    }
   },
 };
 
@@ -202,8 +213,8 @@ const library = {
 
 // console.log("------");
 
-library.addTrackToPlaylist("t03", "p01");
-console.log(library.playlists);
+// library.addTrackToPlaylist("t03", "p01");
+// console.log(library.playlists);
 
 // console.log("------");
 
@@ -219,4 +230,5 @@ console.log(library.playlists);
 // // where the name, artist or album contains the query string (case insensitive)
 // // tip: use "string".search("tri")
 // // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/search
-// const printSearchResults = function (query) {};
+
+//printSearchResults(/ode/i);
